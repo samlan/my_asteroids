870d08881d4b909484e1aff3838f550bf8db7833
samlan <samuel.lando@gmail.com>
2025-10-27 01:13:23 +0300

    drawing a player to screen

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 
 
@@ -12,6 +14,7 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    ply = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     pygame.init()
     clock = pygame.time.Clock()
@@ -21,6 +24,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        ply.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
