d550fa1c60ce4cc282758a299e3d12140157349d
samlan <samuel.lando@gmail.com>
2025-10-26 23:43:48 +0300

    refactoring and simplifying code

--- main.py
+++ main.py
@@ -4,28 +4,26 @@
 import pygame
 from constants import *
 
-pygame.init()
-pygame.time.Clock()
-dt = 0
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#while True:
-#	for event in pygame.event.get():
-#		 if event.type == pygame.QUIT:
-#			 return
-#	pygame.Surface.fill(screen,(0,0,0))
-#	pygame.display.flip()
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = pygame.time.Clock().tick(60) / 1000
-    pygame.Surface.fill(screen,(0,0,0))
-    pygame.display.flip()
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
