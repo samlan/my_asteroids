023eb1bb170e7f5cfed151239514dc835299b05e
samlan <samuel.lando@gmail.com>
2025-10-29 23:48:07 +0300

    adding object groups

--- main.py
+++ main.py
@@ -5,28 +5,48 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    ply = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-
+    
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (updatable, drawable)
+    ply = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+
     dt = 0
+  
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        ply.update(dt)
-        screen.fill((0,0,0))
-        #ply.update(10)
-        ply.draw(screen)
+        
+        updatable.update(dt)
+        #ply.update(dt)
+        
+        screen.fill("black")
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+      #  ply.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
