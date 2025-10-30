d708a416530b80f1ca1a93338b54e8158de6ba0a
samlan <samuel.lando@gmail.com>
2025-10-30 23:28:20 +0300

    adding final changes on main

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if shot.collides_with(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         #ply.update(dt)
         
